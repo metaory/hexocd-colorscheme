dd2749c0753f8590d1fbd13548068ef5817b643b
metaory <metaory@gmail.com>
2024-12-06 23:12:55 +0800

    1206_2312

--- README.sh
+++ README.sh
@@ -28,15 +28,13 @@ markup <<EOF >>README.md
     { "align": "center" },
     [
       "img",
-      { "src": ".github/assets/icon.webp", "height": "120" }
+      { "src": "/.github/assets/icon.webp", "height": "120" }
     ],
     ["h1", "HE̶̶X̶̶OCD" ]
   ],
   ""
 ]
 EOF
-
-cp README.md templates/
 
 function place {
   jq -f <(
--- templates/README.md
+++ templates/README.md
@@ -7,11 +7,15 @@
   align="center"
 >
   <img
-    src=".github/assets/icon.webp"
+    src="/.github/assets/icon.webp"
     height="120"
    />
   <h1>
     HE̶̶X̶̶OCD
   </h1>
 </div>
-
+Refer to
+<a href="https://github.com/metaory/confsubst">
+  confsubst
+</a>
+for template usage
